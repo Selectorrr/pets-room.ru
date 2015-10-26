72d7ef4c3afd80dca8ded5a80a69a4a7b6716146
Selector <litvinovsd@gmail.com>
2015-10-26 22:33:37 +0300

    Initial commit

---
+++ src/main/java/ru/petsroom/config/metrics/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Health and Metrics specific code.
+ */
+package ru.petsroom.config.metrics;
---
+++ src/main/java/ru/petsroom/web/websocket/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Access Objects used by WebSocket services.
+ */
+package ru.petsroom.web.websocket.dto;
